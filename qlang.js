79832b8997c8a6520dad003d35dc4f86cfb2d4ed
wanderxuhq <wanderxuhq@gmail.com>
2019-06-16 02:53:05 +0800

    变量支持

--- qlang.js
+++ qlang.js
@@ -6,6 +6,15 @@ class QNode{
 		this.type = type;
 		this.value = value;
 	}
+	
+	getValue(variables){
+		if(this.type === 4){
+			return variables[this.value];
+		} else {
+			return this.value;
+		}
+	}
+	
 	static NUM = 1;
 	static CHAR = 2;
 	static STR = 3;
@@ -13,39 +22,26 @@ class QNode{
 	static PNT = 5;
 }
 
-function QStatement(inputs, variables){
-	this.inputs = inputs;
-	this.variables = variables || {};
-	this.pattern = /\s*((?<cmt>\/\/.*)|(?<var>[_$A-Za-z][_\$A-Za-z0-9]*)|(?<num>[0-9]+(\.[0-9]+)?)|(?<str>\"(\\\\|\\\"|\\n|[^\"])*\")|(?<pnt>\*\*|==|!=|<=|>=|&&|\|\||[.,/#!$%^&\*;:{}+-=_`~()><]))?/g;
-	
-	this.run = function(){
-		var nodes = [];
-		do{
-			var m = this.pattern.exec(this.inputs);
-			if (m.groups['var'] !== undefined){
-				//if m.group('var') in QUtil.keywords:
-				//    nodes.append(m.group('var'))
-				//else:
-				nodes.push(new QNode(QNode.VAR, m.groups['var']));
-			} else if (m.groups['num'] != null){
-				nodes.push(new QNode(QNode.NUM, m.groups['num']));
-			} else if (m.groups['str'] != null){
-				nodes.push(new QNode(QNode.STR, m.groups['str']));
-			} else if (m.groups['pnt'] != null){
-				nodes.push(new QNode(QNode.PNT, m.groups['pnt']));
-			}
-		} while(m.index < inputs.length);
-		return this.execute(nodes)[0];
+class QStatement{
+	constructor(nodes, variables){
+		this.nodes = nodes;
+		this.variables = variables || {};
 	}
 	
-	this.execute = function(nodes){
-		console.log(this.inputs, nodes);
+	run(){
+		var x = this.execute(this.nodes)[0];
+		console.log(x, this.variables);
+		return x.value;
+	}
+	
+	execute(nodes){
+		//console.log(this.inputs, nodes);
 		var exp = [{predicate: null}];
 		var length = nodes.length;
 		var n = 0;
+		var skip = 0;
 		while (n < length){
 			var node = nodes[n];
-			console.log(node);
 			if (node.value == '('){
 				[exp[exp.length - 1]['noun'], skip] = this.execute(nodes.slice(n + 1, nodes.length));
 				console.log(exp[exp.length - 1], skip);
@@ -59,7 +55,13 @@ function QStatement(inputs, variables){
 				if (exp[exp.length - 1]['predicate'] !== null && this.Operation[exp[exp.length - 1]['predicate']]['priority'] >= this.Operation[node.value]['priority']){
 					for(var i = exp.length - 1; i > 0; i--){
 						console.log('hit!');
-						exp[i - 1]['noun'] = new QNode(QNode.NUM, this.Operation[exp[i]['predicate']]['apply'](parseFloat(exp[i - 1]['noun'].value), parseFloat(exp[i]['noun'].value)));
+						//debugger;
+						if(/[\+\-\*\/]|\*\*/.test(exp[i]['predicate'])){
+							exp[i - 1]['noun'] = new QNode(QNode.NUM, this.Operation[exp[i]['predicate']]['apply'](parseFloat(exp[i - 1]['noun'].getValue(this.variables)), parseFloat(exp[i]['noun'].getValue(this.variables))));
+						} else {
+							//debugger;
+							exp[i - 1]['noun'] = new QNode(QNode.NUM, this.Operation[exp[i]['predicate']]['apply'](exp[i - 1]['noun'].value, exp[i]['noun'].getValue()))
+						}
 					}
 					exp = [{'predicate': null, 'noun': exp[0]['noun']}];
 				}
@@ -70,21 +72,63 @@ function QStatement(inputs, variables){
 		if (exp.length > 1){
 			for(var i = exp.length - 1; i > 0; i--){
 				console.log('hit!');
-				exp[i - 1]['noun'] = new QNode(QNode.NUM, this.Operation[exp[i]['predicate']]['apply'](parseFloat(exp[i - 1]['noun'].value), parseFloat(exp[i]['noun'].value)))
+				//debugger;
+				if(/[\+\-\*\/]|\*\*/.test(exp[i]['predicate'])){
+					exp[i - 1]['noun'] = new QNode(QNode.NUM, this.Operation[exp[i]['predicate']]['apply'](parseFloat(exp[i - 1]['noun'].getValue(this.variables)), parseFloat(exp[i]['noun'].getValue(this.variables))))
+				} else {
+					//debugger;
+					exp[i - 1]['noun'] = new QNode(QNode.NUM, this.Operation[exp[i]['predicate']]['apply'](exp[i - 1]['noun'].value, exp[i]['noun'].getValue()))
+				}
 			}
 		}
 		console.log(exp[0], length);
-		return [exp[0]['noun'], length];	
+		return [exp[0]['noun'], length];
 	}
 	
-	this.Operation = {
+	Operation = {
 		'+': {'priority': 1, 'apply': (x, y) => x + y},
 		'-': {'priority': 1, 'apply': (x, y) => x - y},
 		'*': {'priority': 2, 'apply': (x, y) => x * y},
 		'/': {'priority': 2, 'apply': (x, y) => x / y},
 		'**': {'priority': 3, 'apply': (x, y) => x ** y},
-		'=': {'priority': 0, 'apply': (x, y) => {this.variables[variable] = value;}}
+		'=': {'priority': 0, 'apply': (x, y) => {this.variables[x] = y;}}
 	}
 }
 
+class QBlock{
+	pattern = /\s*((?<cmt>\/\/.*)|(?<var>[_$A-Za-z][_\$A-Za-z0-9]*)|(?<num>[0-9]+(\.[0-9]+)?)|(?<str>\"(\\\\|\\\"|\\n|[^\"])*\")|(?<pnt>\*\*|==|!=|<=|>=|&&|\|\||[.,/#!$%^&\*;:{}+-=_`~()><]))?/g;
+	constructor(input, variables){
+		this.input = input;
+		this.variables = variables || {};
+	}
+	
+	run(){
+		var nodes = [[]];
+		var index = 0;
+		do{
+			var m = this.pattern.exec(this.input);
+			if (m[0] === ';'){
+				index++;
+				nodes[index] = [];
+			} else if (m.groups['var'] !== undefined){
+				//if m.group('var') in QUtil.keywords:
+				//    nodes.append(m.group('var'))
+				//else:
+				nodes[index].push(new QNode(QNode.VAR, m.groups['var']));
+			} else if (m.groups['num'] !== undefined){
+				nodes[index].push(new QNode(QNode.NUM, m.groups['num']));
+			} else if (m.groups['str'] !== undefined){
+				nodes[index].push(new QNode(QNode.STR, m.groups['str']));
+			} else if (m.groups['pnt'] !== undefined){
+				nodes[index].push(new QNode(QNode.PNT, m.groups['pnt']));
+			}
+		} while(m.index < this.input.length);
+		var x;
+		console.log(nodes);
+		for(var i = 0; i < nodes.length; i++){
+			x = new QStatement(nodes[i], this.variables).run();
+		}
+		return x;
+	}
+}
 //})();
