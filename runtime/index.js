10541516d19a838ada2564466ad4333295347cbb
wanderxuhq <wanderxuhq@gmail.com>
2024-11-09 17:09:49 +0800

    Add native function to array and adjust runValue logic

--- runtime/index.js
+++ runtime/index.js
@@ -1,4 +1,7 @@
 import parse from '../parser/index.js';
+import { PrimeType } from '../type/constant.js';
+import { Void } from '../value/constant.js';
+import { fromNative, toNative } from './native.js';
 import runStatements from './run-statements.js';
 
 let context = new Map();
@@ -6,12 +9,23 @@ let context = new Map();
 context.set('String', { name: 'String', type: 'Type' });
 context.set('Int', { name: 'Int', type: 'Type' });
 context.set('Void', { name: 'Void', type: 'Type' });
-context.set('print', { type: 'Function', system: true, call: e => {
-    let value = e;
-    if (typeof(e) !== 'string') {
-        value = JSON.stringify(e)
-    }
-    process.stdout.write(value)
-}, parameters: [{variable: 'p'}] });
+context.set('print', fromNative(
+    e => {
+        let value = toNative(e.result.value);
+        if (typeof (value) !== 'string') {
+            value = JSON.stringify(value)
+        }
+        process.stdout.write(value);
 
-runStatements({ context: context, scope: new Map() })(parse('./demo/obj2.ql'));
+        return Void;
+    }
+).value);
+
+context.set('Arrays', fromNative({
+    add: (arr, e) => {
+        arr.result.value.values.push(e.result);
+        return Void
+    }
+}).value);
+
+runStatements({ context: context, scope: new Map() })(parse('./demo/exp.ql'));
--- runtime/native.js
+++ runtime/native.js
@@ -1,8 +1,9 @@
+import { Ast } from "../ast/index.js";
 import { PrimeType } from "../type/constant.js";
 
 const toNative = (ast) => {
     if (ast.type === PrimeType.Array) {
-        return ast.values.map(e => toNative(e.result.value));
+        return ast.values.map(e => toNative(e.value));
     } else if (ast.type === PrimeType.Boolean) {
         return ast.value
     } else if (ast.type === PrimeType.Number) {
@@ -17,6 +18,66 @@ const toNative = (ast) => {
     }
 }
 
+const trueTypeOf = (obj) => Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
+
+const fromNative = (value) => {
+    const type = trueTypeOf(value);
+    if (type === 'boolean') {
+        return {
+            type: Ast.VALUE,
+            value: {
+                type: PrimeType.Boolean,
+                value: value
+            }
+        }
+    } else if (type === 'number') {
+        return {
+            type: Ast.VALUE,
+            value: {
+                type: PrimeType.Number,
+                value: value
+            }
+        }
+    } else if (type === 'string') {
+        return {
+            type: Ast.VALUE,
+            value: {
+                type: PrimeType.String,
+                value: value
+            }
+        }
+    } else if (type === 'object') {
+        let fields = [];
+        for (const field in value) {
+            fields.push(
+                {
+                    variable: {
+                        type: Ast.IDENTITY,
+                        value: field
+                    },
+                    value: fromNative(value[field])
+                }
+            )
+        }
+        return {
+            type: Ast.VALUE,
+            value: {
+                type: PrimeType.Object,
+                fields: fields
+            }
+        }
+    } else if (type === 'function') {
+        return {
+            type: Ast.VALUE,
+            value: {
+                type: 'Function',
+                system: true,
+                call: value,
+            }
+        }
+    }
+}
+
 export {
-    toNative
+    toNative, fromNative
 }
